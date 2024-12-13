e6d4a8f8d87ce80a31c540226bdcc6f423394dd9
Istvan Peter Jaray <istvanpeterjaray@gmail.com>
2024-12-13 16:27:10 +0100

    ADD: turned async on

--- mav_scraper/pipeline.py
+++ mav_scraper/pipeline.py
@@ -5,7 +5,9 @@ def main():
     pipeline = HungarianRailwayScraperPipeline()
 
     # Run the scraper workflow
-    pipeline.run(limit=None)  # Set `limit=None` to process all trains
+    pipeline.run(
+        limit=None, save_format="parquet", async_mode=True
+    )  # Set `limit=None` to process all trains
 
 
 if __name__ == "__main__":
